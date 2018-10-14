b46c5e4399fd865ca953303ed5ec9468ba1bf80d
jortvangorkum <jortvangorkum@gmail.com>
2018-10-14 13:17:51 +0200

    Added extra information

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+-- Graphics Library: IO Version of Gloss
+import Graphics.Gloss.Interface.IO.Game
+
+{-
+  Settings for Display
+-}
+
+window :: Display
+window = InWindow "Pac-Man" (400, 400) (0,0)
+
+background :: Color
+background = black
+
+frames :: Int
+frames = 10
+
+{-
+  Main function
+-}
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = playIO window background frames
+              undefined       -- Initial state
+              undefined       -- View function
+              undefined       -- Event function
+              undefined       -- Step function
