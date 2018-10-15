f8c8a5d14503493e632bd0fac98bb7cbbdaeae57
Vince van Noort <vincevannoort@gmail.com>
2018-10-15 13:39:46 +0200

    added default grid

--- src/Model.hs
+++ src/Model.hs
@@ -9,8 +9,47 @@ module Model where
 {-
   Initialization
 -}
+initialGameGrid :: Grid
+initialGameGrid = Grid 28 31 [
+  W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W,
+  W, D, D, D, D, D, D, D, D, D, D, D, D, W, W, D, D, D, D, D, D, D, D, D, D, D, D, W,
+  W, D, W, W, W, W, D, W, W, W, W, W, D, W, W, D, W, W, W, W, W, D, W, W, W, W, D, W,
+  W, D, W, W, W, W, D, W, W, W, W, W, D, W, W, D, W, W, W, W, W, D, W, W, W, W, D, W,
+  W, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, W,
+  W, D, W, W, W, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, W, W, W, D, W,
+  W, D, W, W, W, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, W, W, W, D, W,
+  W, D, D, D, D, D, D, W, W, D, D, D, D, W, W, D, D, D, D, W, W, D, D, D, D, D, D, W,
+  W, W, W, W, W, W, D, W, W, W, W, W, D, W, W, D, W, W, W, W, W, D, W, W, W, W, W, W,
+  E, E, E, E, E, W, D, W, W, W, W, W, D, W, W, D, W, W, W, W, W, D, W, E, E, E, E, E,
+  E, E, E, E, E, W, D, W, W, D, D, D, D, D, D, D, D, D, D, W, W, D, W, E, E, E, E, E,
+  E, E, E, E, E, W, D, W, W, D, W, W, W, E, E, W, W, W, D, W, W, D, W, E, E, E, E, E,
+  W, W, W, W, W, W, D, W, W, D, W, E, E, E, E, E, E, W, D, W, W, D, W, W, W, W, W, W,
+  D, D, D, D, D, D, D, D, D, D, W, E, E, E, E, E, E, W, D, D, D, D, D, D, D, D, D, D,
+  W, W, W, W, W, W, D, W, W, D, W, E, E, E, E, E, E, W, D, W, W, D, W, W, W, W, W, W,
+  E, E, E, E, E, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, E, E, E, E, E,
+  E, E, E, E, E, W, D, W, W, D, D, D, D, D, D, D, D, D, D, W, W, D, W, E, E, E, E, E,
+  E, E, E, E, E, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, E, E, E, E, E,
+  W, W, W, W, W, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, W, W, W, W, W,
+  W, D, D, D, D, D, D, D, D, D, D, D, D, W, W, D, D, D, D, D, D, D, D, D, D, D, D, W,
+  W, D, W, W, W, W, D, W, W, W, W, W, D, W, W, D, W, W, W, W, W, D, W, W, W, W, D, W,
+  W, D, W, W, W, W, D, W, W, W, W, W, D, W, W, D, W, W, W, W, W, D, W, W, W, W, D, W,
+  W, D, D, D, W, W, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, W, W, D, D, D, W,
+  W, W, W, D, W, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, W, D, W, W, W,
+  W, W, W, D, W, W, D, W, W, D, W, W, W, W, W, W, W, W, D, W, W, D, W, W, D, W, W, W,
+  W, D, D, D, D, D, D, W, W, D, D, D, D, W, W, D, D, D, D, W, W, D, D, D, D, D, D, W,
+  W, D, W, W, W, W, W, W, W, W, W, W, D, W, W, D, W, W, W, W, W, W, W, W, W, W, D, W,
+  W, D, W, W, W, W, W, W, W, W, W, W, D, W, W, D, W, W, W, W, W, W, W, W, W, W, D, W,
+  W, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, W,
+  W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W
+  ] 
+  where 
+    W = Wall
+    D = PacDot
+    E = Empty
+
+
 initialState :: GameState
-initialState = GameState 0 Playing Chase (Grid 60 60 []) (PacMan 3 (Position 0 0)) [Blinky (Position 0 0), Pinky (Position 0 0), Inky (Position 0 0), Clyde (Position 0 0)]
+initialState = GameState 0 Playing Chase initialGameGrid (PacMan 3 (Position 0 0)) [Blinky (Position 0 0), Pinky (Position 0 0), Inky (Position 0 0), Clyde (Position 0 0)]
 
 secondsBetweenCycles :: Float
 secondsBetweenCycles = 5
